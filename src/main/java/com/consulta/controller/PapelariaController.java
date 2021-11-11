3dba06271735917703f39e19596dcc12e81dd2af
ErickAlves7K <erickhue1@hotmail.com>
2021-11-11 17:48:44 -0300

    Adicionando função de adicionar materiais

--- src/main/java/com/consulta/controller/PapelariaController.java
+++ src/main/java/com/consulta/controller/PapelariaController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.consulta.beans.Papelaria;
@@ -23,9 +25,19 @@ public class PapelariaController {
 		return ResponseEntity.ok(list);
 	
 	}
-	@GetMapping("/papelaria/{id}")
+	@GetMapping("/papelaria/{id}") //função para consultar pelo ID
 	public ResponseEntity<Optional<Papelaria>> getById(@PathVariable("id") int id) {
 		Optional<Papelaria> idList = dao.findById(id);
 		return ResponseEntity.ok(idList);
+	}
+	@PostMapping("/novomaterial") //função para adicionar um material
+	public ResponseEntity<Papelaria> addMaterial(@RequestBody Papelaria material) {
+		try {
+			dao.save(material);
+			return ResponseEntity.ok(material);
+		} catch(Exception e) {
+			e.printStackTrace();
+			return ResponseEntity.status(403).build();
+		}
 	}
 }
