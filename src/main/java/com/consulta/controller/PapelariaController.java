329c73927bdad7e0f4dab9f8570fd8a8528d9175
Anubis7K <erickhue1@hotmail.com>
2021-11-10 23:28:42 -0300

    Adicionando consulta

--- src/main/java/com/consulta/controller/PapelariaController.java
+++ src/main/java/com/consulta/controller/PapelariaController.java
@@ -1,10 +1,11 @@
 package com.consulta.controller;
 
 import java.util.List;
-
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.consulta.beans.Papelaria;
@@ -20,5 +21,11 @@ public class PapelariaController {
 	public ResponseEntity<List<Papelaria>> GetAll() {
 		List<Papelaria> list = (List<Papelaria>)dao.findAll();
 		return ResponseEntity.ok(list);
+	
+	}
+	@GetMapping("/papelaria/{id}")
+	public ResponseEntity<Optional<Papelaria>> getById(@PathVariable("id") int id) {
+		Optional<Papelaria> idList = dao.findById(id);
+		return ResponseEntity.ok(idList);
 	}
 }
